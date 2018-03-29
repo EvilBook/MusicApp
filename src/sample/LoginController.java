8ee9390877919f2202db3c7b7ec2656952facafa
Jjoogam0001 <jjoogam@gmail.com>
2018-03-29 20:09:23 +0200

    Wazuuuppp

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -8,7 +8,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import sample.DatabaseConnection.ThisIsForConnecting;
 
 import javax.imageio.IIOException;
 import java.io.IOException;
@@ -17,17 +20,38 @@ import java.util.ResourceBundle;
 
 public class LoginController implements Initializable{
 
-    @FXML private Button loginButton;
-    @FXML private Button createButton;
+
+    @FXML private TextField userNameTextField;
+    @FXML private PasswordField PasswordTextField;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
+
     }
 
     @FXML
     public void handleLoginButton(ActionEvent event){
+
+        Exceptions myapp = new Exceptions();
+        try {
+            myapp.UsernameException(userNameTextField.getText());
+            myapp.MaximumPasswordLength(PasswordTextField.getText());
+            PasswordTextField.clear();
+            userNameTextField.clear();
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 
