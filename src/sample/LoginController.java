620291df78cb8af5bde13e165a7dbb45ff4d734f
Jjoogam0001 <jjoogam@gmail.com>
2018-03-29 21:12:29 +0200

    ExceptionClassRemoved(Under Construction)

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -7,12 +7,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import javax.imageio.IIOException;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -34,16 +34,7 @@ public class LoginController implements Initializable{
     @FXML
     public void handleLoginButton(ActionEvent event){
 
-        Exceptions myapp = new Exceptions();
-        try {
-            myapp.UsernameException(userNameTextField.getText());
-            myapp.MaximumPasswordLength(PasswordTextField.getText());
-            PasswordTextField.clear();
-            userNameTextField.clear();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
 
 
