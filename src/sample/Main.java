c0042e1cb5e2ca61da96e47dc1aad7d9e7d7121a
Andrei <Louliett@gmail.com>
2018-03-30 21:25:43 +0200

    playing with CSS for the login screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,24 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import sample.DatabaseConnection.ThisIsForConnecting;
 
 public class Main extends Application {
 
-    @FXML private Button loginButton;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("Vinyl Store");
+        primaryStage.setTitle("MasMusic");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
-        ThisIsForConnecting connect=new ThisIsForConnecting();
+        ThisIsForConnecting connect = new ThisIsForConnecting();
 
     }
 
