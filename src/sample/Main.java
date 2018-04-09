936cc8a235cd5214c78c9c494313ff2dffcf76d6
Cassian <Louliett@gmail.com>
2018-04-09 16:43:11 +0200

    updated master 09.04.2018

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sample.DatabaseConnection.ThisIsForConnecting;
 
-public class Main extends Application {
+public class Main  extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
