307c3b1c3addc5044859848b6823e9363d3a4e3a
EvilBook <martin.zannato@yahoo.com>
2018-04-09 22:25:41 +0200

    started user scene

--- src/sample/MainUserScreenController.java
+++ src/sample/MainUserScreenController.java
@@ -1,5 +1,9 @@
 package sample;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,9 +24,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import sample.DatabaseConnection.RetrieveInfoFromDatabase;
 import sample.DatabaseConnection.UpdateDatabase;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -50,39 +56,85 @@ public class MainUserScreenController implements Initializable {
     }
 
 
+    ArrayList<String> names=new ArrayList<>();
+
 
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ReadNames();
 
-        horizontalMenu.setSpacing(8);
-        horizontalMenu.setMinWidth(400*horizontalMenu.getSpacing());
+        horizontalMenu.setSpacing(14);
+        horizontalMenu.setMinWidth(400*names.size());
         horizontalMenu.setAlignment(Pos.CENTER_LEFT);
-        for(int i=0;i<14;i++) {
+        for(int i=0;i<names.size();i++) {
             ImageView imageView=new ImageView();
-            Image image=new Image(getClass().getResource("andrei.png").toString());
+            Image image=new Image(getClass().getResource("Graphics/Records/"+names.get(i)).toString());
 
             imageView.setImage(image);
-            imageView.setFitWidth(300);
+            imageView.setFitWidth(250);
             imageView.setPreserveRatio(true);
             imageView.setSmooth(true);
             imageView.setCache(true);
             imageView.setOnMouseEntered(event -> {
-                imageView.setFitWidth(400);
+                Timeline time=new Timeline();
+                KeyValue kv = new KeyValue(imageView.fitWidthProperty(), 350, Interpolator.EASE_BOTH);
+                KeyFrame kf = new KeyFrame(Duration.seconds(0.15), kv);
+                time.getKeyFrames().add(kf);
+                time.setOnFinished(t->{
+                    // remove pane and restore scene 1
+                    //root1.getChildren().setAll(rectangle1);
+                    // set scene 2
+                    //primaryStage.setScene(scene2);
+                });
+                time.play();
             });
             imageView.setOnMouseExited(event -> {
-                imageView.setFitWidth(300);
+                Timeline time=new Timeline();
+                KeyValue kv = new KeyValue(imageView.fitWidthProperty(), 250, Interpolator.EASE_BOTH);
+                KeyFrame kf = new KeyFrame(Duration.seconds(0.15), kv);
+                time.getKeyFrames().add(kf);
+                time.setOnFinished(t->{
+                    // remove pane and restore scene 1
+                    //root1.getChildren().setAll(rectangle1);
+                    // set scene 2
+                    //primaryStage.setScene(scene2);
+                });
+                time.play();
             });
             horizontalMenu.getChildren().add(imageView);
         }
 
         moveLeft.setOnMouseClicked(event -> {
-            horizontalMenu.setTranslateX(horizontalMenu.getTranslateX()+80);
+            //horizontalMenu.setTranslateX(horizontalMenu.getTranslateX()+80);
+            Timeline time=new Timeline();
+            KeyValue kv = new KeyValue(horizontalMenu.layoutXProperty(),horizontalMenu.getLayoutX()+400, Interpolator.EASE_BOTH);
+            KeyFrame kf = new KeyFrame(Duration.seconds(0.4), kv);
+            time.getKeyFrames().add(kf);
+            time.setOnFinished(t->{
+                // remove pane and restore scene 1
+                //root1.getChildren().setAll(rectangle1);
+                // set scene 2
+                //primaryStage.setScene(scene2);
+            });
+            time.play();
         });
         moveRight.setOnMouseClicked(event -> {
-            horizontalMenu.setTranslateX(horizontalMenu.getTranslateX()-80);
+            Timeline time=new Timeline();
+            KeyValue kv = new KeyValue(horizontalMenu.layoutXProperty(),horizontalMenu.getLayoutX()-400, Interpolator.EASE_BOTH);
+            KeyFrame kf = new KeyFrame(Duration.seconds(0.4), kv);
+            time.getKeyFrames().add(kf);
+            time.setOnFinished(t->{
+                // remove pane and restore scene 1
+                //root1.getChildren().setAll(rectangle1);
+                // set scene 2
+                //primaryStage.setScene(scene2);
+            });
+            time.play();
         });
+
+
 
 
 
@@ -97,23 +149,20 @@ public class MainUserScreenController implements Initializable {
         RetrieveInfoFromDatabase newRetrieve=new RetrieveInfoFromDatabase();
         name.setText(newRetrieve.getName(userEmail));
     }
-    public AnchorPane addAnchorPane(AnchorPane grid) {
+    public ArrayList<String> ReadNames() {
+        File folder = new File("C:\\Users\\NoFox\\Downloads\\MusicApp\\src\\sample\\Graphics\\Records");
+        File[] listOfFiles = folder.listFiles();
+        System.out.println(folder.listFiles());
 
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+                names.add(listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+        return names;
 
-        AnchorPane anchorpane = new AnchorPane();
-        Button buttonSave = new Button("Save");
-        Button buttonCancel = new Button("Cancel");
-
-        HBox hb = new HBox();
-        hb.setPadding(new Insets(0, 10, 10, 10));
-        hb.setSpacing(10);
-        hb.getChildren().addAll(buttonSave, buttonCancel);
-
-        anchorpane.getChildren().addAll(grid,hb);   // Add grid from Example 1-5
-        AnchorPane.setBottomAnchor(hb, 8.0);
-        AnchorPane.setRightAnchor(hb, 5.0);
-        AnchorPane.setTopAnchor(grid, 10.0);
-
-        return anchorpane;
     }
 }
