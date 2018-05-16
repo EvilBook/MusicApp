fe278e2d78c691f122fa66a6f3135eb159e5c7f5
Jjoogam0001 <jjoogam@gmail.com>
2018-05-16 14:36:38 +0200

    Forgotten Password

--- src/sample/ForgottenPasswordController.java
+++ src/sample/ForgottenPasswordController.java
@@ -1,14 +1,16 @@
 package sample;
 
+
+
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+
 import javafx.fxml.Initializable;
+
 import javafx.scene.control.*;
+
 import sample.DatabaseConnection.PersonDBConnection;
-import sample.Employee.Album;
-import sample.Employee.AlbumNameViewClass;
-
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -20,154 +22,295 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.Properties;
+import java.util.Random;
 import java.util.ResourceBundle;
-import java.util.concurrent.ExecutionException;
-
 
 public class ForgottenPasswordController implements Initializable {
 
+
+
     @FXML
+
     private Label ConfirmationMessage;
 
 
+
+
+
     @FXML
+
     private TextField EmailTextfield;        // receipients email
 
-    private String USER_NAME = "jjoogam";  // GMail user name (just the part before "@gmail.com")
-    private String PASSWORD = "nakabiri"; // GMail password
 
-    String from = USER_NAME;
-    String pass = PASSWORD;
+
+    private String USER_NAME = "evenmoremasm";  // GMail user name (just the part before "@gmail.com")
+
+    private String PASSWORD = "moremasm1!"; // GMail password
+
+
+
+    private String email = "";
+
+
+
     String subject = "Password Recovery";
 
 
-    String body = "Hello Martin ....this is freaking owesome";
+
     ResultSet resultSet;
 
-    String to [] = {"jjoogam@gmail.com"};
+
+
+
+
+
 
     @Override
+
     public void initialize(URL location, ResourceBundle resources) {
+
+
+
 
 
     }
 
 
+
+
+
     @FXML
+
     private void handleSendPasswordButton(ActionEvent event) throws IOException {
+
+
+
+        email = EmailTextfield.getText();
+
+
+
+
 
         sendFromGMail(USER_NAME,PASSWORD,subject);
 
+    }
 
 
 
 
 
+    public String generateRandom(){
+
+        Random r = new Random();
+
+
+
+        final String alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        final int N = alphabet.length();
+
+        String str = "";
+
+
+
+
+
+        for(int i=0; i < 8; i++){
+
+
+
+            Character ch = alphabet.charAt(r.nextInt(N));
+
+            str = str + ch;
 
         }
+
+
+
+        return str;
+
+    }
+
+
+
+
+
+
 
     private  void sendFromGMail(String from, String pass, String subject) {
 
 
 
-
-        String sql = "select password from login where Email = '"+ EmailTextfield.getText() +"';";
-
-
-
-
-
-
-
-
-
-
+        String to [] = {email};
 
 
 
         Properties props = System.getProperties();
+
         String host = "smtp.gmail.com";
+
+
 
         props.put("mail.smtp.starttls.enable", "true");
 
+
+
         props.put("mail.smtp.ssl.trust", host);
+
         props.put("mail.smtp.user", from);
+
         props.put("mail.smtp.password", pass);
+
         props.put("mail.smtp.port", "587");
+
         props.put("mail.smtp.auth", "true");
 
 
+
+
+
         Session session = Session.getDefaultInstance(props);
+
         MimeMessage message = new MimeMessage(session);
 
+
+
         PersonDBConnection personDBConnection = new PersonDBConnection();
-       Statement statement = null;
+
+        Statement statement = null;
+
+
+
 
 
         try {
 
+            String sql = "select password from login where Email = '"+ email +"';";
+
+
+
             statement = personDBConnection.connection.createStatement();
+
             resultSet = statement.executeQuery(sql);
 
 
+
             String passwd = null;
-            while (resultSet.next()) {
+
+
+
+
+
+            //Checks if there is a result
+
+            if(resultSet.next()) {
+
+
+
                 passwd = resultSet.getString(1);
+
                 System.out.println(resultSet.getString(1));
 
 
+
+
+
+                message.setFrom(new InternetAddress(from));
+
+                InternetAddress[] toAddress = new InternetAddress[to.length];
+
+
+
+                // To get the array of addresses
+
+                for (int i = 0; i < to.length; i++) {
+
+                    toAddress[i] = new InternetAddress(to[i]);
+
+                }
+
+
+
+                for (int i = 0; i < toAddress.length; i++) {
+
+                    message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+
+                }
+
+
+
+                //Change password to generated one
+
+                String generatePw = generateRandom();
+
+                String sql2 = "UPDATE login SET password = '"+ generatePw + "' WHERE Email = '"+ email +"';";
+
+                statement.executeUpdate(sql2);
+
+
+
+                System.out.println("GenPw:" + generatePw + " Email: " + email);
+
+
+
+
+
+                message.setSubject(subject);
+
+                message.setText(generatePw);
+
+
+
+
+
+                Transport transport = session.getTransport("smtp");
+
+
+
+
+
+                transport.connect(host, from, pass);
+
+                transport.sendMessage(message, message.getAllRecipients());
+
+                transport.close();
+
+                System.out.println("Message sent");
+
+                ConfirmationMessage.setText("Email with Your Password is sent to " + email);
+
+                EmailTextfield.clear();
+
             }
 
+            else{
 
-            message.setFrom(new InternetAddress(from));
-            InternetAddress[] toAddress = new InternetAddress[to.length];
+                ConfirmationMessage.setText("The Email '" + email +"' Is Not Registered.");
 
-            // To get the array of addresses
-            for (int i = 0; i < to.length; i++) {
-                toAddress[i] = new InternetAddress(to[i]);
             }
-
-            for (int i = 0; i < toAddress.length; i++) {
-                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
-            }
-
-
-            message.setSubject(subject);
-            message.setText(passwd);
-
-
-            Transport transport = session.getTransport("smtp");
-
-
-            transport.connect(host, from, pass);
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-            System.out.println("Message sent");
-            ConfirmationMessage.setText("Email with Your Password is sent to " + EmailTextfield.getText());
-            EmailTextfield.clear();
 
         }
+
         catch (AddressException ae) {
+
             ae.printStackTrace();
+
         }
+
         catch (MessagingException me) {
+
             me.printStackTrace();
+
         }
+
         catch (SQLException e){
+
             e.printStackTrace();
+
         }
-    }
-
-    @FXML
-    private void handleReturnButton(ActionEvent event) throws IOException{
-
-        SwitchScene back = new SwitchScene();
-        back.logOut(event);
 
     }
-
 
 
 
 }
-
